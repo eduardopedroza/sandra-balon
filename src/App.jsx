72678ab0468eb79919e0ff60d7925f319fce9039
EduardoPedroza <ea.pedroza23@gmail.com>
2025-02-10 12:45:57 -0600

    Setup entire app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
+import TopProducts from "./components/TopProducts/TopProducts";
+import Footer from "./components/Footer/Footer";
+import Testimonials from "./components/Testimonials/Testimonials";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
-  return <div>App</div>;
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Products />
+      <TopProducts />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
