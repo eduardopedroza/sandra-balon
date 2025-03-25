4a06acae95270b4a22bfaadbf329656aea4cde17
EduardoPedroza <ea.pedroza23@gmail.com>
2025-03-25 16:54:12 -0600

    blocked unnecessary components

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Products />
-      <TopProducts />
+      {/* <TopProducts /> */}
       <Testimonials />
       <Footer />
     </div>
