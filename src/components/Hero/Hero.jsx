801c09f0f503b949ae46f9500c17f99f33bbbc68
EduardoPedroza <ea.pedroza23@gmail.com>
2025-06-02 16:53:17 -0600

    updated wrong farmacy

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -159,7 +159,7 @@ const Hero = ({ handleOrderPopup }) => {
                   </h1>
                   <p className="text-2xl mt-2">
                     <span className="text-green-500 font-bold">
-                      Mediformulas: ${data.price.mediformulas.toLocaleString()}{" "}
+                      PharmaX: ${data.price.mediformulas.toLocaleString()}{" "}
                       (hasta {data.price.discount}% de ahorro)
                     </span>
                     <br />
